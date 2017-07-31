c2e1900c25478dc302d693806f6cd6fea3c4e901
Abdi Suleman <abdisuleman12@gmail.com>
2017-07-31 08:42:59 -0500

    hw all the way trough basemode

--- client.js
+++ client.js
@@ -16,32 +16,46 @@ $('.container').append('<h3> ' +  peopleName + ':' + ' ' + peopleShoutOut + ' </
 
 // created a counter variable 
 counter = 0
+
 $('#Next').on('click', function() {
     
     counter  = (counter + 1)  % peopleArray.length;
-
+    //$('.container').fadeOut('slow', function (){
+    //When a person is displayed, show their first name, last name and their shout out. Only one person should be shown at any given time.
     $('.container').html('<h3> ' +  peopleArray[counter].name + ':'  + ' ' + peopleArray[counter].shoutout + ' </h3>');
+   // });
 
-     $('#number-display').html('<h2>' + (counter + 1) + ' / ' + 23 + '</h2>');
+    //Also on the DOM should be a display showing the number of people and which is being currently viewed (eg. 2/20).
+     $('#number-display').html('<h3>' + (counter + 1) + ' / ' + 23 + '</h3>');
 
          
     });
 
       
     counter = peopleArray.length
-    $('#Prev').on('click', function () {
-        counter = (counter - 1) % peopleArray.length ;
+    
+$('#Prev').on('click', function () {
+    
+    counter = (counter - 1) % peopleArray.length ;
         if (counter ===  -1 ) {
             counter = peopleArray.length - 1
             
         }
-
+    
+    //When a person is displayed, show their first name, last name and their shout out. Only one person should be shown at any given time.
     $('.container').html('<h3> ' +  peopleArray[counter].name + ':'  + ' ' + peopleArray[counter].shoutout + ' </h3>');
     
-    $('#number-display').html('<h2>' + (counter + 1) + ' / ' + 23 + '</h2>');
+    //Also on the DOM should be a display showing the number of people and which is being currently viewed (eg. 2/20).
+    $('#number-display').html('<h3>' + (counter + 1) + ' / ' + 23 + '</h3>');
 
 
     });
 
 
-});
+
+})
+
+
+
+
+
